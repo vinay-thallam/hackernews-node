26ab05f99cc87ae9787a7e8620b58f314b547e02
vinay thallam <vinay.rgit@gmail.com>
2020-03-17 21:22:28 +0530

    support queriying for list of feeds

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ const {GraphQLServer} = require('graphql-yoga')
 const typeDefs = `
 type Query{
     info: String!
+    feed: [Link!]!
+}
+type Link{
+    id: ID!
+    description: String!
+    url: String!
 }`
 
 const resolvers = {
     Query: {
-        info: () => 'This is the API for HackerNews Clone 2'
+        info: () => 'This is the API for HackerNews Clone 2',
+        feed: () => links
+    },
+    Link: {
+        id: (parent) => parent.id,
+        url: (parent) => parent.url,
+        description: (parent) => parent.description,
     }
 }
 
@@ -15,5 +27,11 @@ const server = new GraphQLServer({
     typeDefs, 
     resolvers
 })
+
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+}]
 
 server.start(() => console.log(`server is running on http://localhost:4000`))
