50f6336ff3c7b4a418e4af686f97aa4ded16fa1b
vinay thallam <vinay.rgit@gmail.com>
2020-03-17 22:52:47 +0530

    Link crud

--- src/index.js
+++ src/index.js
@@ -1,32 +1,58 @@
 const {GraphQLServer} = require('graphql-yoga')
 
-const typeDefs = `
-type Query{
-    info: String!
-    feed: [Link!]!
-}
-type Link{
-    id: ID!
-    description: String!
-    url: String!
-}`
-
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL New'
+}]
+let idCount = links.length
 const resolvers = {
     Query: {
         info: () => 'This is the API for HackerNews Clone 2',
-        feed: () => links
+        feed: () => links,
+        link: (parent, {id}) => links.filter(link => link.id === id)[0]
+    },
+    Mutation: {
+        post: (parent, {description, url}) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description,
+                url
+            }
+            links.push(link)
+            return link
+        },
+        updateLink: (parent, {id, description, url}) => {
+            let updatedLink = {}
+            links = links.map((link) => {
+
+                if(link.id === id){
+                    return updatedLink = {
+                        id,
+                        description: description || link.description,
+                        url: url || link.url,
+                    }
+                }
+                return {...link} 
+            })
+            return updatedLink
+        },
+        deleteLink: (parent, {id}) => {
+            let deletedLink = links.filter(link => link.id === id)[0]
+            links = links.reduce((acc, link) => {
+                if(link.id !== id){
+                    acc.push(link)
+                }
+                return acc
+            }, [])
+            return deletedLink
+        }
     }
 }
 
 const server = new GraphQLServer({
-    typeDefs, 
+    typeDefs: './src/schema.graphql', 
     resolvers
 })
-
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}]
 
 server.start(() => console.log(`server is running on http://localhost:4000`))
