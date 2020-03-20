c377eb0c2e82147f827b68dabc67eac7e60a5f9a
vinay thallam <vinay.rgit@gmail.com>
2020-03-20 17:29:44 +0530

    paginate feed

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,11 +1,15 @@
-function feed(parent, args, context, info) {
+async function feed(parent, args, context, info) {
     const where = args.filter ? {
         OR: [
             {description_contains: args.filter},
             {url_contains: args.filter}
         ]
     } : {}
-    return context.prisma.links({where})
+    return await context.prisma.links({
+        where,
+        skip: args.skip,
+        first: args.first
+    })
 }
 
 module.exports = {
